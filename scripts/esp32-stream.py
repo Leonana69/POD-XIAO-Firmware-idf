ad6832832f66bb27e51a10a7e915b9d6a2646eca
Guojun Chen <guojun.chen@yale.edu>
2024-04-04 16:34:36 -0400

    add python scripts

---
+++ scripts/esp32-stream.py
@@ -0,0 +1,17 @@
+import json
+from podtp import Podtp
+import time
+
+def main():
+    with open('config.json', 'r') as file:
+        config = json.loads(file.read())
+    
+    podtp = Podtp(config)
+    if podtp.connect():
+        podtp.start_stream()
+        podtp.esp32_echo()
+        time.sleep(4)
+        podtp.disconnect()
+
+if __name__ == '__main__':
+    main()
