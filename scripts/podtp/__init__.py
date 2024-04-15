cf15feb214ae91a6eba2c68d892cefe89af43ae4
Guojun Chen <guojun.chen@yale.edu>
2024-04-15 16:50:38 -0400

    remove python files

--- scripts/podtp/__init__.py
+++
@@ -1,3 +0,0 @@
-from .podtp_packet import PodtpPacket, PodtpType, PodtpPort
-from .podtp import Podtp
-from .utils import print_t
--- scripts/podtp/utils.py
+++
@@ -1,6 +0,0 @@
-import datetime
-
-def print_t(*args, **kwargs):
-    # Print with timestamp
-    current_time = datetime.datetime.now().strftime('%H:%M:%S.%f')[:-3]
-    print(f"[{current_time}]", *args, **kwargs)
